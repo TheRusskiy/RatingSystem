cb9484dcb24ed59a3ebb1073c9fe1e8e69777c40
TheRusskiy <dimanrussian@gmail.com>
2013-10-14 22:09:42 +0400

    directory restructure

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$CONFS['sql_host'] 		= 'localhost';
-$CONFS['sql_database'] 	= 'diploma';
-$CONFS['sql_user'] 		= 'TheRusskiy';
-$CONFS['sql_pass'] 		= 'wordpass';
-$CONFS['sql_port']		= '';
-
-
-?>
--- sources/privity/privity/uio_spravochnikKorpusov.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "core/skin.php";
-include "uio_spravochnikKorpusovForm.php";
- echo fSkin ("Управление имущественными отношениями","<br><a href='uio_inventarizaciya.php'>Инвентаризация </a>"."<br><a href='uio_spravochniki.php'>   Введение&nbspсправочников</a>".
-"<br><a href='uio_otcheti.php'>Отчеты</a>",korpus());
-?>
