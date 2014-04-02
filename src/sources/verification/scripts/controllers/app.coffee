b3a9b82f4a17144baf96a34848452bcf3dd6d6dc
TheRusskiy <dimanrussian@gmail.com>
2014-04-02 12:19:29 +0400

    redirect to home if not logged in

--- src/sources/verification/scripts/controllers/app.coffee
+++ src/sources/verification/scripts/controllers/app.coffee
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('verificationApp')
-  .controller 'AppCtrl', ($scope, $http, Auth, $rootScope, $location)->
+  .controller 'AppCtrl', ($scope, $http, Auth, $rootScope, $location, $window)->
     unless $rootScope.currentUser
       Auth.currentUser().$promise.then( (user)->
         console.log user
@@ -9,6 +9,9 @@ angular.module('verificationApp')
           $rootScope.currentUser = user;
         else
           $rootScope.currentUser = null;
+          alert('Пользователь в текущей сессии не найден, перенаправляем..')
+          new_path = $location.protocol()+"://"+$location.host()+":"+$location.port()
+          $window.location.href = new_path
       ).catch( (err)->
         console.log('Current user:'+err.data);
   #        $scope.errors.other = err.message;
--- src/sources/verification/scripts/controllers/app.js
+++ src/sources/verification/scripts/controllers/app.js
@@ -1,13 +1,17 @@
 (function() {
   'use strict';
-  angular.module('verificationApp').controller('AppCtrl', function($scope, $http, Auth, $rootScope, $location) {
+  angular.module('verificationApp').controller('AppCtrl', function($scope, $http, Auth, $rootScope, $location, $window) {
     if (!$rootScope.currentUser) {
       Auth.currentUser().$promise.then(function(user) {
+        var new_path;
         console.log(user);
         if (user.id != null) {
           return $rootScope.currentUser = user;
         } else {
-          return $rootScope.currentUser = null;
+          $rootScope.currentUser = null;
+          alert('Пользователь в текущей сессии не найден, перенаправляем..');
+          new_path = $location.protocol() + "://" + $location.host() + ":" + $location.port();
+          return $window.location.href = new_path;
         }
       })["catch"](function(err) {
         return console.log('Current user:' + err.data);
