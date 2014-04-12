2bb925bcdaac99ed46ee9038be10a6b0da72dbd2
TheRusskiy <dimanrussian@gmail.com>
2014-04-13 01:55:10 +0400

    User input validation for criteria

--- src/sources/verification/scripts/controllers/criteria_edit.coffee
+++ src/sources/verification/scripts/controllers/criteria_edit.coffee
@@ -22,3 +22,52 @@ angular.module("verificationApp").controller "CriteriaEditCtrl", ($scope, Criter
     return unless confirm("Вы уверены? Удалённый показатель восстановлению не подлежит!")
     Criteria.delete(c)
     $rootScope.goto('/criteria')
+  $scope.revalidateFetchValue = (form)->
+    # required to force validation on fetch value
+    form.fetch_value.$setViewValue(form.fetch_value.$viewValue)
+    null
+
+  deleteEmptyElements = (arr)->
+    i = 0
+    len = arr.length
+    while i < len
+      arr[i] and arr.push(arr[i])
+      i++
+    arr.splice(0 , len);
+    arr
+  isInt = (n)->
+    return parseFloat(n) is parseInt(n, 10) and not isNaN(n)
+  $scope.fetchValueErrors = []
+  $scope.validateFetchValue = ($value)->
+    $scope.fetchValueErrors = []
+    if $scope.criteria.fetch_type == 'manual_options'
+      value = $value
+      multi = ($scope.criteria.multiplier||"").toString()
+      console.log 'validating:'
+      console.log value
+      console.log multi
+      values = value.split('|')
+      multis = multi.split('|')
+      oldLength = values.length
+      no_empty_elements = deleteEmptyElements(values).length == oldLength
+      correctCount = values.length == multis.length
+      formatMatches = value.match /(.+\|)*.+/
+      unless correctCount
+        $scope.fetchValueErrors.push("Число элементов во множителе и данных не совпадает")
+      unless !!formatMatches
+        $scope.fetchValueErrors.push("Некорректный формат данных. Данные должны следовать формату (.+\\|)*.+")
+      unless no_empty_elements
+        $scope.fetchValueErrors.push("Не должно быть пустых элементов")
+      correctCount && !!formatMatches && no_empty_elements
+    else
+      true
+  $scope.validateMultiplierValue = ($value)->
+    $value = $value.toString()
+    values = $value.split('|')
+    valid = true
+    for v in values
+      unless isInt(v)
+        valid = false
+    if $scope.criteria.fetch_type != 'manual_options'
+      valid = false unless values.length is 1
+    valid
--- src/sources/verification/scripts/controllers/criteria_edit.js
+++ src/sources/verification/scripts/controllers/criteria_edit.js
@@ -1,6 +1,7 @@
 (function() {
   "use strict";
   angular.module("verificationApp").controller("CriteriaEditCtrl", function($scope, Criteria, $routeParams) {
+    var deleteEmptyElements, isInt;
     $scope.fetch_types = Criteria.fetch_types();
     $scope.calculation_types = Criteria.calculation_types();
     $scope.id = $routeParams.id;
@@ -21,12 +22,78 @@
       $scope.criteria = Criteria.upsert(c);
       return null;
     };
-    return $scope.deleteCriteria = function(c) {
+    $scope.deleteCriteria = function(c) {
       if (!confirm("Вы уверены? Удалённый показатель восстановлению не подлежит!")) {
         return;
       }
       Criteria["delete"](c);
       return $rootScope.goto('/criteria');
+    };
+    $scope.revalidateFetchValue = function(form) {
+      form.fetch_value.$setViewValue(form.fetch_value.$viewValue);
+      return null;
+    };
+    deleteEmptyElements = function(arr) {
+      var i, len;
+      i = 0;
+      len = arr.length;
+      while (i < len) {
+        arr[i] && arr.push(arr[i]);
+        i++;
+      }
+      arr.splice(0, len);
+      return arr;
+    };
+    isInt = function(n) {
+      return parseFloat(n) === parseInt(n, 10) && !isNaN(n);
+    };
+    $scope.fetchValueErrors = [];
+    $scope.validateFetchValue = function($value) {
+      var correctCount, formatMatches, multi, multis, no_empty_elements, oldLength, value, values;
+      $scope.fetchValueErrors = [];
+      if ($scope.criteria.fetch_type === 'manual_options') {
+        value = $value;
+        multi = ($scope.criteria.multiplier || "").toString();
+        console.log('validating:');
+        console.log(value);
+        console.log(multi);
+        values = value.split('|');
+        multis = multi.split('|');
+        oldLength = values.length;
+        no_empty_elements = deleteEmptyElements(values).length === oldLength;
+        correctCount = values.length === multis.length;
+        formatMatches = value.match(/(.+\|)*.+/);
+        if (!correctCount) {
+          $scope.fetchValueErrors.push("Число элементов во множителе и данных не совпадает");
+        }
+        if (!formatMatches) {
+          $scope.fetchValueErrors.push("Некорректный формат данных. Данные должны следовать формату (.+\\|)*.+");
+        }
+        if (!no_empty_elements) {
+          $scope.fetchValueErrors.push("Не должно быть пустых элементов");
+        }
+        return correctCount && !!formatMatches && no_empty_elements;
+      } else {
+        return true;
+      }
+    };
+    return $scope.validateMultiplierValue = function($value) {
+      var v, valid, values, _i, _len;
+      $value = $value.toString();
+      values = $value.split('|');
+      valid = true;
+      for (_i = 0, _len = values.length; _i < _len; _i++) {
+        v = values[_i];
+        if (!isInt(v)) {
+          valid = false;
+        }
+      }
+      if ($scope.criteria.fetch_type !== 'manual_options') {
+        if (values.length !== 1) {
+          valid = false;
+        }
+      }
+      return valid;
     };
   });
 
