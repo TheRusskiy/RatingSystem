170e4fead8e6f9569677a0b17493001173ed8663
TheRusskiy <dimanrussian@gmail.com>
2014-04-28 11:55:55 +0400

    Use bower components foq 'faq'

--- src/sources/faq/views/head.php
+++ src/sources/faq/views/head.php
@@ -16,18 +16,20 @@
 <!-- FREAKING Windows 1251 ENCODING, screw u-->
     <meta http-equiv=Content-Type content='text/html; charset=UTF-8'>
   <title>Rating system</title>
-    <link type='text/css' href='/sources/faq/styles/reset.css' rel='stylesheet'>
-    <link type='text/css' href='/sources/styles/style.css' rel='stylesheet'>
-    <link type='text/css' href='/sources/faq/styles/jquery-ui-1.10.3.custom.min.css' rel='stylesheet'>
+<!--    <link type='text/css' href='/sources/faq/styles/reset.css' rel='stylesheet'>-->
+    <link type='text/css' href='/sources/faq/bower_components/jquery-ui/themes/base/minified/jquery-ui.min.css' rel='stylesheet'>
+    <link type='text/css' href='/sources/faq/bower_components/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet'>
+    <link type='text/css' href='/sources/faq/styles/style.css' rel='stylesheet'>
     <link type='text/css' href='/sources/faq/styles/rating_system.css' rel='stylesheet'>
     <?= $style ?>
     <!--[if lt IE 9]>
-    <script src="/sources/faq/scripts/html5shiv.js"></script>
-    <script src="/sources/faq/scripts/html5shiv-printshiv.js"></script>
+    <script src="/sources/faq/bower_components/es5-shim/es5-shim.js"></script>
+    <script src="/sources/faq/bower_components/json3/lib/json3.min.js"></script>
     <![endif]-->
-    <script src="/sources/faq/scripts/jquery-1.10.2.min.js"></script>
-    <script src="/sources/faq/scripts/jquery-ui-1.10.3.custom.min.js"></script>
-    <script src="/sources/faq/scripts/modernizr-2.6.3.custom.js"></script>
+    <script src="/sources/faq/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/sources/faq/bower_components/jquery-ui/ui/minified/jquery-ui.min.js"></script>
+    <script src="/sources/faq/bower_components/jquery-ui/ui/i18n/jquery.ui.datepicker-ru.js"></script>
+    <script src="/sources/faq/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="/sources/faq/scripts/rating_system.js"></script>
     <?= $script ?>
 </head>
