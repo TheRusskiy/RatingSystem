5d889ca142c937913ffac55b12149e902c9733e8
TheRusskiy <dimanrussian@gmail.com>
2014-04-28 12:16:20 +0400

    header style

--- src/sources/faq/views/header.php
+++ src/sources/faq/views/header.php
@@ -11,24 +11,43 @@ function class_for($controller){
 ?>
 <body>
 <div class="header">
-    <ul class = 'menu'>
-        <li id='logo'><img src="sources/faq/images/ssaulogo.gif"/></li>
-        <li class="<?= class_for('home')?>">
-            <a href='/?controller=home&action=index'>Рейтинговая cистема</a>
-        </li>
-        <li class="<?= class_for('teachers')?>">
-            <a href='/?controller=teachers&action=index'>Преподаватели</a>
-        </li>
-        <li class="<?= class_for('criteria')?>">
-            <a href='/?controller=criteria&action=index'>Критерии</a>
-        </li>
-        <li>
-            <b><?= $user?></b> |
-            <a href='/?controller=sessions&action=logout'>Выйти</a>
-        </li>
-    </ul>
+    <div class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span
+                        class="icon-bar"></span>
+                </button>
+                <a href='/?controller=home&action=index' class="navbar-brand">
+                    <img id="ssau-logo" src="sources/verification/images/ssaulogo.gif" alt="logo"/>
+                    Рейтинг ППС
+                </a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="<?= class_for('home')?>">
+                        <a href='/?controller=home&action=index'>Рейтинговая система</a>
+                    </li>
+                    <li class="<?= class_for('teachers')?>">
+                        <a href='/?controller=teachers&action=index'>Преподаватели</a>
+                    </li>
+                    <li class="<?= class_for('criteria')?>">
+                        <a href='/?controller=criteria&action=index'>Критерии</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href><?= $user?></a>
+                    </li>
+                    <li>
+                        <a href='/?controller=sessions&action=logout'>Выйти</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
-<div class="content">
+<div class="container">
 <?php if($notice): ?>
     <div class='notice'><?= $notice?></div>
 <?php endif ?>
