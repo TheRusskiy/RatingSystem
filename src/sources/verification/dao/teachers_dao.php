e17e90fbfb3ed0b6b754f0335a85695cf7c09470
TheRusskiy <dimanrussian@gmail.com>
2014-05-15 02:34:25 +0400

    rate filter for teacher dao in verification

--- src/sources/verification/dao/teachers_dao.php
+++ src/sources/verification/dao/teachers_dao.php
@@ -5,6 +5,12 @@ class TeachersDao {
         $teachers_query = mysql_query("
             SELECT s.*
             FROM staff2 s
+            JOIN depstaff ds ON
+            (s.id = ds.id_staff
+            AND ds.rateacc > 0.6
+            )
+            JOIN dep d ON
+            (d.id = ds.id_dep)
             ");
         $teachers = array();
         while ($row = mysql_fetch_array($teachers_query)){
